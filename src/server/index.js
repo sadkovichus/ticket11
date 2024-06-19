16213935f70f3aafac864543ad1da66426638b00
Andrey Smirnoff <j75yhciamoc@gmail.com>
2024-06-19 19:04:05 +0200

    server

--- src/server/index.js
+++ src/server/index.js
@@ -7,45 +7,46 @@ const prisma = new PrismaClient();
 const bcrypt = require('bcrypt')
 
 const app = express();
+const PORT = 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/admin', (req, res) => {
-    const { gender, name, guiltyFrom, guiltyTill, person, clas, username, password } = req.body;
-    const hashedPassword = bcrypt.hash(password, 10)
-    try{
-        prisma.user.create({
-            data: { gender, name, guiltyFrom, guiltyTill, person, clas, username, hashedPassword },
-        });
-        res.json({message: "user saved succesfullt"})
-    }catch(error){
-        console.log("error saving data:", error)
-        res.status(500).json({message: "Internal server error"})
-    }
-
+app.post('/admin', async (req, res) => {
+  const { email, password } = req.body;
+  console.log(password)
+  
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+      data: { email, password},
+    });
+    res.json({ message: "User saved successfully" });
+  } catch (error) {
+    console.log("Error saving data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        const user = await prisma.user.findUnique({
-            where: { email },
-        });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-        if (user && await bcrypt.compare(password, user.password)) {
-            res.json({ message: 'Login successful', user });
-        } else {
-            res.status(401).json({ message: 'Invalid email or password' });
-        }
-    } catch (error) {
-        console.error('Error logging in user:', error);
-        res.status(500).json({ message: 'Internal server error' });
+    if (user && await bcrypt.compare(password, user.password)) {
+      res.json({ message: 'Login successful', user });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
     }
+  } catch (error) {
+    console.error('Error logging in user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
-
-app.listen(process.env.PORT, () => {
-    console.log("Server is running on http://localhost:" + process.env.PORT);
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
 });
