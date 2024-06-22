15e8cc570636665871f11d5cffbf080b3fae0fcf
Andrey Smirnoff <j75yhciamoc@gmail.com>
2024-06-22 23:29:57 +0200

    front-end

--- front-end/src/widgets/auth/create-user/CreateUser.tsx
+++ front-end/src/widgets/auth/create-user/CreateUser.tsx
@@ -14,7 +14,7 @@ const CreateUser = () => {
     setLoading(true);
     const data = await Object.fromEntries(new FormData(e.currentTarget));
     await axios
-      .post('https://ticket-1-0q9a.onrender.com/admin', data)
+      .post('https://ticket-mvib.onrender.com/admin', data)
       .then(function (response) {
         console.log(response);
       })
