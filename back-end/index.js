4793a3eed5a8ad90d6e2abb09a935c6a6ffa90c5
SMOK <kali@localhost.localdomain>
2024-06-23 13:40:57 +0200

    main

--- back-end/index.js
+++ back-end/index.js
@@ -4,15 +4,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = 8000;
 
+// Настройка CORS с конкретным доменом вашего фронтенда
 const corsOptions = {
-  origin: 'https://ticket-nfsf.vercel.app/', // замените на ваш домен
+  origin: 'https://ticket-nfsf.vercel.app/auth', // замените на ваш домен
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
@@ -20,12 +21,12 @@ app.use(bodyParser.json());
 
 app.post('/admin', async (req, res) => {
   const { email, password } = req.body;
-  console.log(password)
+  console.log(password);
   
   try {
     // const hashedPassword = await bcrypt.hash(password, 10);
     await prisma.user.create({
-      data: { email, password},
+      data: { email, password },
     });
     res.json({ message: "User saved successfully" });
   } catch (error) {
@@ -35,57 +36,51 @@ app.post('/admin', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
   
-    try {
-      const user = await prisma.user.findUnique({
-        where: { email },
-      });
-  
-      if (!user) {
-        console.log('User not found for email:', user.email
-        );
-        console.log(email)
-        console.log(user.email)
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      // const passwordMatch = await bcrypt.compare(password, user.password);
-  
-      if (user.email && password
-      ) {
-        res.json({ message: 'Login successful', user });
-      } else {
-        console.log('Invalid password for email:', email);
-        console.log(email)
-        console.log(user.email)
-        res.status(401).json({ message: 'Invalid email or password' });
-      }
-    } catch (error) {
-      console.error('Error logging in user:', error);
-      res.status(500).json({ message: 'Internal server error' });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    
+    if (!user) {
+      console.log('User not found for email:', email);
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
-  });
-  
-  app.get('/profile', async(req, res) => {
-    const { email } = req.body;
-  
-    try {
-      const user = await prisma.user.findUnique({
-        where: { email }
-      });
-  
-      if (user) {
-        res.json(user);
-      } else {
-        res.status(404).json({ message: "User not found" });
-      }
-    } catch (error) {
-      console.error('Error fetching user:', error);
-      res.status(500).json({ message: 'Internal server error' });
+
+    // const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (user.email && password) {
+      res.json({ message: 'Login successful', user });
+    } else {
+      console.log('Invalid password for email:', email);
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
-  });
+  } catch (error) {
+    console.error('Error logging in user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/profile', async (req, res) => {
+  const { email } = req.body;
   
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email }
+    });
+    
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
